c528d15147a4652649c2e31d1b660f3268edc5c3
IgorAlbert <igor.albert@retest.de>
2019-04-23 15:41:56 +0200

    Test the behavior of the method

--- src/test/java/de/retest/recheck/ignore/SearchFilterFilesTest.java
+++ src/test/java/de/retest/recheck/ignore/SearchFilterFilesTest.java
@@ -1,11 +1,16 @@
 package de.retest.recheck.ignore;
 
+import static de.retest.recheck.configuration.ProjectConfiguration.RETEST_PROJECT_CONFIG_FOLDER;
+import static de.retest.recheck.configuration.ProjectConfiguration.RETEST_PROJECT_ROOT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 class SearchFilterFilesTest {
 
@@ -14,5 +19,26 @@ class SearchFilterFilesTest {
 		final List<File> defaultFilterFiles = SearchFilterFiles.getDefaultFilterFiles();
 		assertThat( defaultFilterFiles.stream().map( File::getName ) ).contains( "positioning.filter",
 				"visibility.filter" );
+	}
+
+	@Test
+	void getCustomerFilterFiles_should_only_get_filter_files( @TempDir final Path temp ) throws Exception {
+		final Path configFolder = temp.resolve( RETEST_PROJECT_ROOT );
+		Files.createDirectory( configFolder );
+		final Path retestFolder = configFolder.resolve( RETEST_PROJECT_CONFIG_FOLDER );
+		Files.createDirectory( retestFolder );
+		final Path filterFolder = retestFolder.resolve( "filter" );
+		Files.createDirectory( filterFolder );
+		System.setProperty( RETEST_PROJECT_ROOT, filterFolder.toString() );
+
+		Files.createTempFile( filterFolder, "random", ".ignore" ).toFile();
+		final File colorFilter = Files.createTempFile( filterFolder, "color", ".filter" ).toFile();
+		final File webFontFilter = Files.createTempFile( filterFolder, "web-font", ".filter" ).toFile();
+
+		final List<File> costumerFilterFiles = SearchFilterFiles.getCustomerFilterFiles();
+		assertThat( costumerFilterFiles ).allMatch( file -> file.toString().endsWith( ".filter" ) );
+		assertThat( costumerFilterFiles.stream().map( File::getName ) ).contains( colorFilter.getName().toString(),
+				webFontFilter.getName().toString() );
+
 	}
 }
