3b0061d3117da542ffb06dcd89c18a20ea9009df
IvanKaradzhov <ivan.karadzhov@retest.de>
2019-05-15 21:32:45 +0200

    Creating a method for filter names and making a test

--- src/main/java/de/retest/recheck/ignore/SearchFilterFiles.java
+++ src/main/java/de/retest/recheck/ignore/SearchFilterFiles.java
@@ -2,23 +2,26 @@ package de.retest.recheck.ignore;
 
 import static de.retest.recheck.configuration.ProjectConfiguration.FILTER_FOLDER;
 
-import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import de.retest.recheck.configuration.ProjectConfiguration;
+import de.retest.recheck.review.ignore.io.Loaders;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SearchFilterFiles {
-
-	private static final String FILES_ENDING = ".filter";
+	public static final String FILTER_EXTENSION = "filter";
 	private static final String BASIC_FILTER_DIR = "/filter/";
 	private static final String WEB_FILTER_DIR = BASIC_FILTER_DIR + "web/";
 	private static final List<String> defaultWebFilter =
@@ -30,27 +33,50 @@ public class SearchFilterFiles {
 		return ProjectConfiguration.getInstance().findProjectConfigFolder().resolve( FILTER_FOLDER );
 	}
 
-	public static List<File> getDefaultFilterFiles() {
+	public static List<Path> getAllFilterFiles() {
+		return Stream.concat( getDefaultFilterFiles().stream(), getProjectFilterFiles().stream() ) //
+				.collect( Collectors.toList() );
+	}
+
+	public static List<Path> getDefaultFilterFiles() {
 		return defaultWebFilter.stream() //
 				.map( filter -> SearchFilterFiles.class.getResource( filter ) ) //
 				.filter( Objects::nonNull ) //
-				.map( resource -> new File( resource.getFile() ) ) //
+				.map( resource -> Paths.get( URI.create( resource.toString() ) ) ) //
 				.collect( Collectors.toList() ); //
 	}
 
-	public static List<File> getProjectFilterFiles() {
-		if ( !resolveFilterPath().toFile().exists() ) {
-			ProjectConfiguration.getInstance().ensureProjectConfigurationInitialized();
-		}
-		try ( Stream<Path> paths = Files.walk( resolveFilterPath() ) ) {
-			return paths.filter( Files::isRegularFile ) //
-					.filter( file -> file.toString().endsWith( FILES_ENDING ) ) //
-					.map( Path::toFile ) //
-					.collect( Collectors.toList() ); //
-		} catch ( final Exception e ) {
-			log.info( "You can create your own filter files in the " + File.separator + ".retest" + File.separator
-					+ "filter folder" );
+	public static List<Path> getProjectFilterFiles() {
+		final Path resolveFilterPath =
+				ProjectConfiguration.getInstance().findProjectConfigFolder().resolve( FILTER_EXTENSION );
+		if ( !resolveFilterPath.toFile().exists() ) {
 			return Collections.emptyList();
 		}
+		try ( Stream<Path> paths = Files.walk( resolveFilterPath ) ) {
+			return paths.filter( Files::isRegularFile ) //
+					.filter( file -> file.toString().endsWith( FILTER_EXTENSION ) ) //
+					.collect( Collectors.toList() ); //
+		} catch ( final IOException e ) {
+			log.error( "Exception accessing user filter folder '{}'.", resolveFilterPath, e );
+			return Collections.emptyList();
+		}
+
+	}
+
+	public static Optional<Filter> searchFilterByName( final String name ) throws IOException {
+		for ( int j = 0; j < getAllFilterFiles().size(); j++ ) {
+			final Path filterPath = getAllFilterFiles().get( j );
+			if ( filterPath.getFileName().toString().equals( name ) ) {
+				log.info( "Loading filter file from path: " + filterPath );
+				try ( final Stream<String> ignoreFileLines = Files.lines( filterPath ) ) {
+					final Filter ignoreApplier = Loaders.load( ignoreFileLines ) //
+							.filter( Filter.class::isInstance ) //
+							.map( Filter.class::cast ) //
+							.collect( Collectors.collectingAndThen( Collectors.toList(), CompoundFilter::new ) );
+					return Optional.of( ignoreApplier );
+				}
+			}
+		}
+		return Optional.empty();
 	}
 }
--- src/test/java/de/retest/recheck/ignore/SearchFilterFilesTest.java
+++ src/test/java/de/retest/recheck/ignore/SearchFilterFilesTest.java
@@ -3,45 +3,88 @@ package de.retest.recheck.ignore;
 import static de.retest.recheck.configuration.ProjectConfiguration.FILTER_FOLDER;
 import static de.retest.recheck.configuration.ProjectConfiguration.RETEST_PROJECT_CONFIG_FOLDER;
 import static de.retest.recheck.configuration.ProjectConfiguration.RETEST_PROJECT_ROOT;
+import static de.retest.recheck.ignore.SearchFilterFiles.FILTER_EXTENSION;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import de.retest.recheck.review.ignore.io.Loaders;
 import de.retest.recheck.util.junit.jupiter.SystemProperty;
 
 class SearchFilterFilesTest {
 
-	@Test
-	void getDefaultFilterFiles_should_get_all_filter_files_from_classpath() {
-		final List<File> defaultFilterFiles = SearchFilterFiles.getDefaultFilterFiles();
-		assertThat( defaultFilterFiles.stream().map( File::getName ) ).contains( "positioning.filter",
-				"visibility.filter" );
-	}
+	private Path filterFolder;
 
-	@Test
-	@SystemProperty( key = RETEST_PROJECT_ROOT )
-	void getProjectFilterFiles_should_only_get_filter_files( @TempDir final Path temp ) throws Exception {
+	@BeforeEach
+	void setUp( @TempDir final Path temp ) throws IOException {
 		final Path configFolder = temp.resolve( RETEST_PROJECT_ROOT );
 		Files.createDirectory( configFolder );
 		final Path retestFolder = configFolder.resolve( RETEST_PROJECT_CONFIG_FOLDER );
 		Files.createDirectory( retestFolder );
-		final Path filterFolder = retestFolder.resolve( FILTER_FOLDER );
+		filterFolder = retestFolder.resolve( FILTER_FOLDER );
 		Files.createDirectory( filterFolder );
+	}
 
+	@Test
+	@SystemProperty( key = RETEST_PROJECT_ROOT )
+	void getAllFilterFiles_should_find_all_filters() throws IOException {
+		System.setProperty( RETEST_PROJECT_ROOT, filterFolder.toString() );
+		final File colorFilter = Files.createTempFile( filterFolder, "color", FILTER_EXTENSION ).toFile();
+		final List<Path> allFilterFiles = SearchFilterFiles.getAllFilterFiles();
+		assertThat( allFilterFiles.stream().map( path -> path.getFileName().toString() ) )
+				.contains( "positioning.filter", "visibility.filter", colorFilter.toPath().getFileName().toString() );
+	}
+
+	@Test
+	void getDefaultFilterFiles_should_get_all_filter_files_from_classpath() {
+		final List<Path> defaultFilterFiles = SearchFilterFiles.getDefaultFilterFiles();
+		assertThat( defaultFilterFiles.stream().map( path -> path.getFileName().toString() ) )
+				.contains( "positioning.filter", "visibility.filter" );
+	}
+
+	@Test
+	@SystemProperty( key = RETEST_PROJECT_ROOT )
+	void getProjectFilterFiles_should_only_get_filter_files() throws IOException {
 		Files.createTempFile( filterFolder, "random", ".ignore" ).toFile();
-		final File colorFilter = Files.createTempFile( filterFolder, "color", ".filter" ).toFile();
-		final File webFontFilter = Files.createTempFile( filterFolder, "web-font", ".filter" ).toFile();
+		final File colorFilter = Files.createTempFile( filterFolder, "color", FILTER_EXTENSION ).toFile();
+		final File webFontFilter = Files.createTempFile( filterFolder, "web-font", FILTER_EXTENSION ).toFile();
 		System.setProperty( RETEST_PROJECT_ROOT, filterFolder.toString() );
 
-		final List<File> projectFilterFiles = SearchFilterFiles.getProjectFilterFiles();
-		assertThat( projectFilterFiles ).allMatch( file -> file.toString().endsWith( ".filter" ) );
-		assertThat( projectFilterFiles.stream().map( File::getName ) ).contains( colorFilter.getName().toString(),
-				webFontFilter.getName().toString() );
+		final List<Path> projectFilterFiles = SearchFilterFiles.getProjectFilterFiles();
+		assertThat( projectFilterFiles ).allMatch( file -> file.toString().endsWith( FILTER_EXTENSION ) );
+		assertThat( projectFilterFiles.stream().map( Path::getFileName ) ).contains( colorFilter.toPath().getFileName(),
+				webFontFilter.toPath().getFileName() );
+	}
+
+	@Test
+	void searchFilterByName_should_return_filter_file() throws IOException {
+		final String name = "positioning.filter";
+		final File positioningFile = Paths.get( "src/main/resources/filter/web/positioning.filter" ).toFile();
+		final Optional<Filter> filter = SearchFilterFiles.searchFilterByName( name );
+		final Stream<String> ignoreFileLines = Files.lines( Paths.get( positioningFile.getPath() ) );
+		final Filter ignoreApplier = Loaders.load( ignoreFileLines ) //
+				.filter( Filter.class::isInstance ) //
+				.map( Filter.class::cast ) //
+				.collect( Collectors.collectingAndThen( Collectors.toList(), CompoundFilter::new ) );
+		assertThat( filter.get() ).isEqualToComparingFieldByFieldRecursively( ignoreApplier );
+	}
+
+	@Test
+	void searchFilterByName_should_not_return_nonexistent_file() throws IOException {
+		final String invalidName = "color.filter";
+		final Optional<Filter> invalidFilter = SearchFilterFiles.searchFilterByName( invalidName );
+		assertThat( invalidFilter ).isEqualTo( Optional.empty() );
 	}
 }
